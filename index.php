fa292e99b741ec541512a4d1dc6c3e50745b5256
gelovani_luka <lukagelovani8@gmail.com>
2018-04-27 17:54:20 +0400

    active
    update active

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">მთავარი<span class="sr-only">(current)</span></a></li>
+        <li><a href="#">მთავარი<span class="sr-only">(current)</span></a></li>
         <li><a href="#">სიახლეები</a></li>
         <li><a href="#">კატეგორიები</a></li>
         <li><a href="#">ჩვენს შესახებ</a></li>
